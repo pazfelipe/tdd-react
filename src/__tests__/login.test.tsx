33c793d5a4f424e1dd3af0868c3c448f961b1fdb
Felipe Paz <xxfelipepaz@gmail.com>
2024-07-23 23:59:07 -0300

    test: validate if there's an input element

--- src/__tests__/login.test.tsx
+++ src/__tests__/login.test.tsx
@@ -1,11 +1,16 @@
 import {render} from '@testing-library/react';
-import "@testing-library/jest-dom"
+import "@testing-library/jest-dom";
 
 const LoginPage = () => <div>Login Page</div>;
 
 describe("<LoginPage>", () => {
   it("should render correctly", () => {
     const {getByText} = render(<LoginPage />);
-    expect(getByText('Login Page')).toBeInTheDocument()
+    expect(getByText('Login Page')).toBeInTheDocument();
+  });
+
+  it("should find text input", () => {
+    const {getByPlaceholderText} = render(<LoginPage />);
+    expect(getByPlaceholderText("Email")).toBeInTheDocument()
   });
 });
