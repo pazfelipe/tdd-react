2a8090c7b1bc62cfd184c9de22a84d5d6e7e88c8
Felipe Paz <xxfelipepaz@gmail.com>
2024-07-24 01:58:56 -0300

    refactor: create login page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,45 @@
+import {ChangeEvent, useRef, useState} from 'react';
+
+const LoginPage = () => {
+  const [value, setValue] = useState("");
+  const [lostFocus, setLostFocus] = useState(false);
+
+  const emailRef = useRef<HTMLInputElement>(null);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+    setLostFocus(false);
+
+    if (!/^test$/.test(e.target.value)) {
+      emailRef.current?.classList.add("error");
+      emailRef.current?.classList.remove("success");
+    } else {
+      emailRef.current?.classList.remove("error");
+      emailRef.current?.classList.add("success");
+    }
+  };
+
+  return (
+    <div>
+      Login Page
+      <div>
+        <input
+          ref={emailRef}
+          placeholder="Email"
+          type="email"
+          name="email"
+          required
+          value={value}
+          onChange={onChange}
+          onFocus={() => setLostFocus(false)}
+          onBlur={() => setLostFocus(true)}
+        />
+        {lostFocus && emailRef.current?.classList.contains("error") && (
+          <span>Please, enter a valid email</span>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage
