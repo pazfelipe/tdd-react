4b10bdc2572779426280aca0fc428cc0ee051ea2
Felipe Paz <xxfelipepaz@gmail.com>
2024-07-23 23:53:16 -0300

    test: create local component to pass test

--- src/__tests__/login.test.tsx
+++ src/__tests__/login.test.tsx
@@ -1,6 +1,7 @@
 import {render} from '@testing-library/react';
 import "@testing-library/jest-dom"
 
+const LoginPage = () => <div>Login Page</div>;
 
 describe("<LoginPage>", () => {
   it("should render correctly", () => {
